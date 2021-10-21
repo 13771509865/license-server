4f25fddf2480c6bbf3678dd0ed95e2c0ff2867f0
zhouf <513829633@qq.com>
2021-10-21 10:56:29 +0800

    1.init:初始化git

---
+++ license-server-api/src/test/java/com/yozosoft/licenseserver/LicenseServerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yozosoft.licenseserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LicenseServerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ license-server-core/src/main/java/com/yozosoft/licenseserver/LicenseServerCoreApplication.java
@@ -0,0 +1,13 @@
+package com.yozosoft.licenseserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicenseServerCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LicenseServerCoreApplication.class, args);
+    }
+
+}
