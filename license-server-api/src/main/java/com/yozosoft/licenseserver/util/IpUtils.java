a35a6ad5200b66d9e2e6875e434c9468d3f99f91
zhouf <513829633@qq.com>
2021-12-08 14:04:38 +0800

    1.fix:空ip转换问题

--- license-server-api/src/main/java/com/yozosoft/licenseserver/util/IpUtils.java
+++ license-server-api/src/main/java/com/yozosoft/licenseserver/util/IpUtils.java
@@ -8,17 +8,24 @@ import java.net.InetAddress;
 public class IpUtils {
 
     public static Integer inetAton(String ip) {
-        InetAddress inetAddress = InetAddresses.forString(ip);
-        return InetAddresses.coerceToInteger(inetAddress);
+        boolean isIp = InetAddresses.isInetAddress(ip);
+        if(isIp){
+            InetAddress inetAddress = InetAddresses.forString(ip);
+            return InetAddresses.coerceToInteger(inetAddress);
+        }
+        return 0;
     }
 
     public static String inetNtoa(Integer ip) {
-        Inet4Address inet4Address = InetAddresses.fromInteger(ip);
-        return InetAddresses.toAddrString(inet4Address);
+        if(ip!=null){
+            Inet4Address inet4Address = InetAddresses.fromInteger(ip);
+            return InetAddresses.toAddrString(inet4Address);
+        }
+        return null;
     }
 
     public static void main(String[] args) {
-        String s = inetNtoa(2130706433);
+        String s = inetNtoa(0);
         System.out.println(s);
     }
 }
