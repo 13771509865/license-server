ab0c4e93c24ad4c54498671decdcfca5bfb33d76
zhouf <513829633@qq.com>
2021-12-22 16:58:11 +0800

    1.feat:修改sha1为hmacsha1

--- license-server-api/src/main/java/com/yozosoft/licenseserver/util/XAuthUtils.java
+++ license-server-api/src/main/java/com/yozosoft/licenseserver/util/XAuthUtils.java
@@ -1,18 +1,20 @@
 package com.yozosoft.licenseserver.util;
 
-import org.apache.commons.codec.digest.DigestUtils;
+import com.google.common.hash.Hashing;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
 /**
  * X-Auth签名头信息工具类
+ *
  * @author zhouf
  */
 public class XAuthUtils {
 
-    public static String buildYozoAuth(Date date, String secret, String contentMd5){
-        String str = secret + contentMd5 + date.toString();
-        String sha1 = DigestUtils.sha1Hex(str);
-        return sha1;
+    public static String buildYozoAuth(Date date, String secret, String contentMd5) {
+        String str = contentMd5 + date.toString();
+        String hmacSha1 = Hashing.hmacSha1(secret.getBytes(StandardCharsets.UTF_8)).hashString(str, StandardCharsets.UTF_8).toString();
+        return hmacSha1;
     }
 }
